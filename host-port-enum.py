c38f1a7612dde755857cbb7f22d609056e04739f
D Ξ Ξ P Λ K <m4xx101101@gmail.com>
2021-02-26 00:50:31 +0530

    Update host-port-enum.py

--- host-port-enum.py
+++ host-port-enum.py
@@ -4,15 +4,6 @@ from datetime import datetime
 import socket
 import threading
 
-HEADER = '\033[95m'
-OKBLUE = '\033[94m'
-OKCYAN = '\033[96m'
-OKGREEN = '\033[92m'
-WARNING = '\033[93m'
-FAIL = '\033[91m'
-ENDC = '\033[0m'
-BOLD = '\033[1m'
-UNDERLINE = '\033[4m'
 protocolname = 'tcp' 
 def pingit(x):
     cmd= "ping -n 1 " + x
