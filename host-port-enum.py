6170ebc844388993e4aa113da9bc3f2e9e61fbfe
D Ξ Ξ P Λ K <m4xx101101@gmail.com>
2021-03-01 15:07:54 +0530

    Update host-port-enum.py

--- host-port-enum.py
+++ host-port-enum.py
@@ -1,13 +1,21 @@
+from ast import parse
 import os
 from os.path import split
 import platform
-from datetime import datetime
 import socket
 import threading
 import numpy as np
 import argparse
 
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+hostip=get_ip_address()
 protocolname = 'tcp'
+newip=[]
+tmp=hostip.split('.')
 print('''
 ██╗  ██╗ ██████╗ ███████╗████████╗   ██████╗  ██████╗ ██████╗ ████████╗   ███████╗███╗   ██╗██╗   ██╗███╗   ███╗
 ██║  ██║██╔═══██╗██╔════╝╚══██╔══╝   ██╔══██╗██╔═══██╗██╔══██╗╚══██╔══╝   ██╔════╝████╗  ██║██║   ██║████╗ ████║
@@ -15,13 +23,15 @@ print('''
 ██╔══██║██║   ██║╚════██║   ██║╚════╝██╔═══╝ ██║   ██║██╔══██╗   ██║╚════╝██╔══╝  ██║╚██╗██║██║   ██║██║╚██╔╝██║
 ██║  ██║╚██████╔╝███████║   ██║      ██║     ╚██████╔╝██║  ██║   ██║      ███████╗██║ ╚████║╚██████╔╝██║ ╚═╝ ██║
 ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝      ╚═╝      ╚═════╝ ╚═╝  ╚═╝   ╚═╝      ╚══════╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝ By m4xx''')
+
 def arguments():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-ip','--ipAddress', dest = "IP", help='Current System IP Address')
-	parser.add_argument('-p','--port',dest = "PORT", help='Port range', default=1-65535)
-	parser.add_argument('-t','--thread',dest = "thread", help='Threads Counts', default=57)
-	args = parser.parse_args()
-	return args
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-ip','--ipAddress', dest = "IP", help='Current System IP Address',default=hostip)
+        parser.add_argument('-p','--port',dest = "PORT", help='Port range', default="1-65535")
+        parser.add_argument('-r','--range',dest = "range", help="Scanning range Example: /16,/24 ", default="/24")
+        parser.add_argument('-t','--thread',dest = "thread", help='Threads Counts', default=57)
+        args = parser.parse_args()
+        return args
 
 args = arguments()
 oper = platform.system()
@@ -31,6 +41,19 @@ elif (oper == "Linux"):
    ping1 = "ping -c 1 "
 else :
    ping1 = "ping -c 1 "
+
+if args.range == "/16":
+    net1=tmp[0]+'.'+tmp[1]+'.'+'0'+'.'+'0'+"/16"
+    for ip in range(1,255):
+        for ip2 in range(1,255):
+            newip.append(tmp[0]+'.'+tmp[1]+'.'+str(ip)+'.'+str(ip2))
+elif args.range == "/24":
+    net1=tmp[0]+'.'+tmp[1]+'.'+tmp[2]+'.'+'0'+"/24"
+    for ip in range(1,255):
+        newip.append(tmp[0]+'.'+tmp[1]+'.'+tmp[2]+'.'+str(ip))
+else:
+    print("Invalid argument for --range,-r\n Try: -r /24")
+
 def pingit(x):
     for k in x:
         cmd= ping1 + k
@@ -50,18 +73,12 @@ def scan(host,ports):
                 socket.socket(socket.AF_INET,socket.SOCK_STREAM).close()
         except:
             pass
-t1 = datetime.now()
 IP = args.IP
-tmp=IP.split('.')
-newip=[]
 alive=[]
-net1=tmp[0]+'.'+tmp[1]+'.'+tmp[2]+'.'+'0'
-for ip in range(1,255):
-    newip.append(tmp[0]+'.'+tmp[1]+'.'+tmp[2]+'.'+str(ip))
-
 workref = []
+
 print('\n'+'='*50 + '\n')
-print("\033[93m\033[1m[!]\033[0m \033[91mStarting Host Discovery For: \033[0m\033[1m\033[94m{}/16\033[0m\n".format(net1))
+print("\033[93m\033[1m[!]\033[0m \033[91mStarting Host Discovery For: \033[0m\033[1m\033[94m{}\033[0m\n".format(net1))
 print('='*50 + '\n')
 split_ip=np.array_split(newip, int(args.thread))
 for k in split_ip:
@@ -77,7 +94,6 @@ print('\033[93m\033[1m[!]\033[0m \033[91m Scanning Port for range \033[0m\033[1m
 print('='*52 + '\n')
 getport=args.PORT.split('-')
 allports=range(int(getport[0]),int(getport[1])+1)
-#print(allports)
 threads=[]
 split_port=np.array_split(allports, int(args.thread))
 for o in alive:
